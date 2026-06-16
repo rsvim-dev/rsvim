eb846a5831f0ca8803f6bcfa37b7245d4fe36bbf
linrongbin16 <linrongbin16@outlook.com>
2026-06-16 14:18:06 +0800

    c

--- rsvim_core/src/js/binding/global_rsvim/fs_tests.rs
+++ rsvim_core/src/js/binding/global_rsvim/fs_tests.rs
@@ -1134,14 +1134,62 @@ async fn test_fs_mkdir1() -> IoResult<()> {
   let terminal_rows = 10_u16;
   let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(30000))];
   let tmp = assert_fs::TempDir::new().unwrap();
-  let target = tmp.child("hello");
+  let target1 = tmp.child("hello");
+  let target2 = tmp.child("world");
 
   let src = format!(
     r#"
   await Rsvim.fs.mkdir({:?});
+  Rsvim.fs.mkdirSync({:?});
   Rsvim.rt.exit();
     "#,
-    target.path(),
+    target1.path(),
+    target2.path(),
+  );
+  info!("src:{:?}", src);
+
+  // Prepare $RSVIM_CONFIG/rsvim.js
+  let _tp = make_configs(vec![(Path::new("rsvim.js"), &src)]);
+
+  let mut event_loop =
+    make_event_loop(terminal_cols, terminal_rows, CliOptions::empty());
+
+  event_loop.initialize()?;
+  event_loop
+    .run_with_mock_events(MockEventReader::new(mocked_events))
+    .await?;
+  event_loop.shutdown()?;
+
+  // After running
+  {
+    let contents = lock!(event_loop.cmdline_text);
+    let n = contents.message_history().len();
+    assert_eq!(n, 0);
+  }
+
+  Ok(())
+}
+
+#[tokio::test]
+#[cfg_attr(miri, ignore)]
+async fn test_fs_mkdir2() -> IoResult<()> {
+  test_log_init();
+
+  let terminal_cols = 10_u16;
+  let terminal_rows = 10_u16;
+  let mocked_events = vec![MockEvent::SleepFor(Duration::from_millis(30000))];
+  let tmp = assert_fs::TempDir::new().unwrap();
+  let target1 = tmp.child("hello").child("world");
+  let target2 = tmp.child("good").child("bye");
+
+  let src = format!(
+    r#"
+  await Rsvim.fs.mkdir({:?}, {{recursive: true, mode: 0o766}});
+  Rsvim.fs.mkdirSync({:?}, {{recursive: true, mode: 0o766}});
+  Rsvim.rt.exit();
+    "#,
+    target1.path(),
+    target2.path(),
   );
   info!("src:{:?}", src);
 
